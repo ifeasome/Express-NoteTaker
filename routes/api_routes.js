4747c8aefce234ad6efda47ef030e86d1ecccf10
Somelina Obiechina <ifeasome@gmail.com>
2020-12-19 14:57:51 -0600

    bug fixes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,76 +1,70 @@
-
 // LOAD DATA
 
 // ===============================================================================
 
-let notesData = require("../db/db.json");
 let fs = require("fs");
 // const { v4: uuidv4 } = require('uuid');
 // const { json } = require("express");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // ---------------------------------------------------------------------------
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     // let newNote = req.body;
     // newNote.id = uuidv4();
     fs.readFile("./db/db.json", (err, data) => {
       if (err) throw err;
-      console.log(data);
+      // console.log(data);
       res.json(JSON.parse(data));
-      
     });
   });
-  
-  
+
   // API POST Requests
   // ---------------------------------------------------------------------------
 
-app.post("/api/notes", function (req, res) {
-    let newNote = fs.readFile("./db/db.json", "utf8")
+  app.post("/api/notes", function (req, res) {
+    let newNote = fs.readFileSync("./db/db.json", "utf8");
     newNote = JSON.parse(newNote);
     newNote.push(req.body);
-    console.log(req.body);
+
+
 
     for (let i = 0; i < newNote.length; i++) {
-  newNote[i].id = i + 1;
-}
-
+      newNote[i].id = i + 1;
+    }
     newNote = JSON.stringify(newNote);
-    fs.writeFile("./db/db.json", newNote);
-    res.json(JSON.parse(newNote));
+    fs.writeFileSync("./db/db.json", newNote);
+
+    res.json(newNote);
   });
 
   // ---------------------------------------------------------------------------
   // API Delete notes
-  app.delete("/api/notes/:id", function(req, res) {
-    let deleteNote = fs.readFile("./db/db.json", "utf8")
+  app.delete("/api/notes/:id", function (req, res) {
     // Empty out the arrays of data
     fs.readFile("./db/db.json", (err, data) => {
       if (err) throw err;
-      console.log(data);
-      res.json(JSON.parse(data)); 
 
-    for (let i = 0; i < deleteNote.length; i++) {
-      if (deleteNote[i].id == req.params.id) {
-        deleteNote.splice(i, 1);
+      let deleteNote = JSON.parse(data);
+
+  
+
+      for (let i = 0; i < deleteNote.length; i++) {
+        if (deleteNote[i].id == req.params.id) {
+          deleteNote.splice(i, 1);
+        }
       }
 
-      
       fs.writeFile("./db/db.json", JSON.stringify(deleteNote), (err) => {
         if (err) throw err;
-        res.send(deleteNote);
         console.log(deleteNote);
+        res.send(true);
+        
       });
-     
-    }
     });
-
-    
   });
-}
+};
